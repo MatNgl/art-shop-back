fa0f36337254b94043732563dd4bf98873c8a61f
MatNgl <mattheo.naegellen@gmail.com>
2026-01-19 23:23:24 +0100

    feat: ajout Swagger pour documentation API

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Art Shop API')
+    .setDescription('API de la plateforme de vente en ligne pour artiste')
+    .setVersion('1.0')
+    .addBearerAuth() // Ajout de l'authentification JWT
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
